a72ef0086b5fa0ac701fa563e0034c84743ee623
Ajit133 <dasajit697@gmail.com>
2023-01-19 23:11:48 +0600

    Catch add

--- src/Hooks/DataFetch.js
+++ src/Hooks/DataFetch.js
@@ -1,24 +1,38 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+const loadingMessage = <p>Todo is Loading....</p>
 const DataFetch = ()=>{
     const [todos,setTodos] = useState(null)
+    const [isLoading,setIsloading] = useState(true) 
+    const [error,setError] = useState(null);
     useEffect(()=>{
             fetch("https://jsonplaceholder.typicode.com/posts")
             .then((res)=>{
-                return res.json()
+                if(!res.ok){
+                    throw Error("Fetching is not successful");
+                }else{
+                return res.json()}
             })
             .then((data)=>{
                 setTodos(data);
+                setIsloading(false)
+                setError(null);
                 
             })
+            .catch((error)=>{
+                setError(error.message);
+                setIsloading(false);
+            });
     },[]);
-    if (todos) 
+    // if (todos) 
+    const todoElements = todos && todos.map((todo)=>{
+        return <p key={todo.id}>{todo.body}</p>
+    })
     return(
          <div>
-            {todos.map((todo)=>{
-                    return <p key={todo.id}>{todo.body}</p>
-                })}
+            {error && <p>{error}</p>}
+            {isLoading && loadingMessage}
+            {todoElements} 
         </div>
     
     )
